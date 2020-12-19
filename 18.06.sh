c6ef8b30df7a7e0cb723753f46fb1a13a3041408
authon <authon@vip.qq.com>
2020-12-19 14:13:32 +0800

    Update 18.06.sh

--- 18.06.sh
+++ 18.06.sh
@@ -51,7 +51,7 @@ git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/node-request
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
-
+svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree
 
 # 生成完整目录清单
 cat >> Update.md <<EOF
