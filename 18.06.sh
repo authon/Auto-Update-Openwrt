5de17431ef91069abd54d8742d1f4e6ec17527b9
authon <authon@vip.qq.com>
2021-04-10 21:28:29 +0800

    Update 18.06.sh

--- 18.06.sh
+++ 18.06.sh
@@ -47,7 +47,6 @@ git clone https://github.com/destan19/OpenAppFilter luci-app-oaf
 git clone https://github.com/littletao08/luci-app-eqos
 git clone https://github.com/iamaluckyguy/luci-app-smartinfo
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
-git clone https://github.com/jerrykuku/node-request
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-ramfree
